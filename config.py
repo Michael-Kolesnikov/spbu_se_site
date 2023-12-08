a12a3254b488a6c1beceea7a15b512abaa8bbbae
Michael-Kolesnikov <kolesnikov_michael@mail.ru>
2023-12-08 09:23:39 +0300

    add config fields for practice

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ import os
 import pathlib
 from datetime import datetime
 
+
 class Config(object):
     SECRET_KEY = os.path.join(
         pathlib.Path(__file__).parent, "configs/flask_se_secret.conf"
@@ -28,7 +29,9 @@ class Config(object):
     FREEZER_IGNORE_MIMETYPE_WARNINGS = True
     # SQLAlchimy config
     SQLALCHEMY_TRACK_MODIFICATIONS = True
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + SQLITE_DATABASE_PATH + "/" + SQLITE_DATABASE_NAME
+    SQLALCHEMY_DATABASE_URI = (
+        "sqlite:///" + SQLITE_DATABASE_PATH + "/" + SQLITE_DATABASE_NAME
+    )
     SECRET_KEY = SECRET_KEY
     SESSION_COOKIE_NAME = "se_session"
     # Secret for API
@@ -45,19 +48,68 @@ class Config(object):
     SIMPLEMDE_JS_IIFE = True
     SIMPLEMDE_USE_CDN = False
     type_id_string = [
-    "",
-    "Bachelor_Report",
-    "Bachelor_Thesis",
-    "Master_Thesis",
-    "Autumn_practice_2nd_year",
-    "Spring_practice_2nd_year",
-    "Autumn_practice_3rd_year",
-    "Spring_practice_3rd_year",
-    "Production_practice",
-    "Pre_graduate_practice",
+        "",
+        "Bachelor_Report",
+        "Bachelor_Thesis",
+        "Master_Thesis",
+        "Autumn_practice_2nd_year",
+        "Spring_practice_2nd_year",
+        "Autumn_practice_3rd_year",
+        "Spring_practice_3rd_year",
+        "Production_practice",
+        "Pre_graduate_practice",
     ]
+    # Yandex disk
 
 
-config = {
-    'default': Config
+YANDEX_CLIENT_ID = "10e079e42b49492295a39e2767e7b049"
+YANDEX_SECRET_FILE = os.path.join(
+    pathlib.Path(__file__).parent, "configs/flask_se_practice_yandex_secret.conf"
+)
+if os.path.exists(YANDEX_SECRET_FILE):
+    with open(YANDEX_SECRET_FILE, "r") as file:
+        YANDEX_SECRET = file.read().rstrip()
+else:
+    YANDEX_SECRET = ""
+
+YANDEX_AUTHORIZE_URL_TEMPLATE = Template(
+    "https://oauth.yandex.ru/authorize?response_type=code"
+    "&client_id=$yandex_client_id&redirect_uri=$redirect_uri"
+)
+YANDEX_GET_TOKEN_URL = "https://oauth.yandex.ru/token"
+
+TABLE_COLUMNS = {
+    "name": "ФИО",
+    "how_to_contact": "Способ оперативной связи (почта, Teams, Telegram, ...)",
+    "supervisor": "Научный руководитель",
+    "consultant": "Консультант (если есть), полностью ФИО, должность и компания",
+    "theme": "Тема",
+    "text": "Текст",
+    "supervisor_review": "Отзыв научника",
+    "reviewer_review": "Отзыв консультанта",
+    "code": "Код",
+    "committer": "Имя коммитера",
+    "presentation": "Презентация",
 }
+
+TEXT_UPLOAD_FOLDER = "static/practice/texts/"
+REVIEW_UPLOAD_FOLDER = "static/practice/reviews/"
+PRESENTATION_UPLOAD_FOLDER = "static/practice/slides/"
+
+ALLOWED_EXTENSIONS = {"pdf"}
+
+MIN_LENGTH_OF_TOPIC = 7
+MIN_LENGTH_OF_GOAL = 20
+MIN_LENGTH_OF_TASK = 15
+
+MIN_LENGTH_OF_FIELD_WAS_DONE = 10
+MIN_LENGTH_OF_FIELD_PLANNED_TO_DO = 10
+
+FORMAT_DATE_TIME = "%d.%m.%Y %H:%M"
+
+# Folders for materials of archive theses
+ARCHIVE_TEXT_FOLDER = "./static/thesis/texts/"
+ARCHIVE_PRESENTATION_FOLDER = "./static/thesis/slides/"
+ARCHIVE_REVIEW_FOLDER = "./static/thesis/reviews/"
+
+config = {"default": Config}
